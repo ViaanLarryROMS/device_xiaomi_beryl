8d5a2e91c23274d7bab1ba5e57756a41cceac143
ViaanROM <viaan152013@gmail.com>
2026-03-08 13:11:52 +0530

    beryl : Adapt for InfinityX

--- infinity_beryl.mk
+++ infinity_beryl.mk
@@ -9,13 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-
-# Stuff
-TARGET_INCLUDE_VIPERFX := false
-TARGET_ENABLE_BLUR := true
-PRODUCT_NO_CAMERA := false
 TARGET_HAS_UDFPS := true
 
 
